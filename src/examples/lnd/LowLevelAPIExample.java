a8e1a3064d398a3deb951b972dc3b9a866a0e02d
herrvendil <info@lightningj.org>
2018-10-20 14:41:53 +0200

    Updated license header to clarify LGPL 3.0 +

--- src/examples/lnd/LowLevelAPIExample.java
+++ src/examples/lnd/LowLevelAPIExample.java
@@ -3,7 +3,8 @@
  *  LightningJ                                                           *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
- *  modify it under the terms of the GNU General Public License          *
+ *  modify it under the terms of the GNU Lesser General Public License   *
+ *  (LGPL-3.0-or-later)                                                  *
  *  License as published by the Free Software Foundation; either         *
  *  version 3 of the License, or any later version.                      *
  *                                                                       *
